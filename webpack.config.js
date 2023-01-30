df1948f7d87c5fb3857046709f570f628a1551d6
Ajeet-jha <jhaajeetcs@gmail.com>
2023-01-30 14:01:55 +0530

    Express server set up

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    target: 'node',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+};
